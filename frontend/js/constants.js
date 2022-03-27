ae13496f58f992175dc497057bf0bc18c5506224
Grammy <grammy.eth@icloud.com>
2022-03-27 00:31:16 -0400

    New Commit Try

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,24 +1,24 @@
 const contractAddress = "0x2F519bA06Af1Cbad6C82B06eB8b5049F00604D18"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
-const welcome_h1 = "Welcome to the Moodles Mob!!";
-const welcome_h2 = "Connect to MetaMask to Get Started";
-const welcome_p = '522 0G Moodles mobbin through the Metaverse. The revolutionary Moodles DAO aims to advance the Web3 ecosystem through  innovative blockchain technology development.';
-const h1_presale_coming_soon = "0G Drop Coming Soon!!";
+const welcome_h1 = "Meet the Moodles Mob!!";
+const welcome_h2 = "Connect to MetaMask Moodle";
+const welcome_p = 'Follow us on the <a href="https://twitter.com/moodlesmob"  target="_blank" rel="noopener noreferrer">Moodles Mob</a> Twitter.';
+const h1_presale_coming_soon = "Moodles Mob Mobbing Soon!!";
 const h1_presale_mint = "Pre-Sale Minting Open!!";
 const h1_public_mint = "Public Minting Open!!";
 const h2_presale_coming_soon = "Pre-Sale Minting Countdown";
 const h2_presale_mint = "Public Minting Countdown";
-const p_presale_coming_soon = "We are working hard to launch the Moodles 1st Generation Collection. Follow Us on Twitter for updates. @moodlesmob!";
+const p_presale_coming_soon = "We are working hard to launch the Moodles Mob 1st Generation Collection. Stay tuned for updates!";
 const p_presale_mint_not_whitelisted = "You are not whitelisted for the pre-sale.. 😢";
 const p_presale_mint_whitelisted = "You're on the whitelist for the pre-sale! 🎉";
 const p_presale_mint_already_minted = "You've already claimed your whitelist mint. Thank you! 🎉";
 const p_public_mint = "No whitelist needed. Public minting is now open! 🎉";
 const button_presale_coming_soon = "Get on the Whitelist";
-const button_presale_mint_whitelisted = "Mint Your Special NFT";
+const button_presale_mint_whitelisted = "Mint Your Moodle";
 const button_presale_mint_not_whitelisted = "Get on the Whitelist";
-const button_presale_already_minted = "Join The Community";
-const button_public_mint = "Mint Your NFT";
+const button_presale_already_minted = "Join The Moodles Mob";
+const button_public_mint = "Mint Your Moodle";
 const mint_failed = "Minting failed. 😢 Please try again.";
 
 const abi = [
